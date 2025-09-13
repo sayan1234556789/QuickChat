a72f2fdb2547bddbcba1ac0b0ef9ba0350e18f03
sayan1234556789 <debsayan466@gmail.com>
2025-09-13 16:00:38 +0530

    third

--- client/src/components/RightSidebar.jsx
+++ client/src/components/RightSidebar.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import assets from "../assets/assets";
 import { AuthContext } from "../context/AuthContext";
-import { ChatContext } from "../context/chatContext";
+import { ChatContext } from "../context/ChatContext";
 import toast from "react-hot-toast";
 import axios from "axios";
 
