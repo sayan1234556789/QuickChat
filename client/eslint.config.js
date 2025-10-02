50276c83eaf52c2902000a08d03480a7d87fb245
sayan1234556789 <debsayan466@gmail.com>
2025-10-02 11:30:13 +0530

    Css

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -3,8 +3,15 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig([
+export default defineConfig([{
+  plugins: [
+    tailwindcss(),
+  ],
+},
+  
   globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
